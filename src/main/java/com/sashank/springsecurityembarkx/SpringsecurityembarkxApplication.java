f1e3ca7047245c06999777ce5467d9456ae29a0b
sashank solasa <sashanksolasa@gmail.com>
2026-05-31 05:22:05 +0530

    added required dependencies and tested basic form based auth

---
+++ src/main/java/com/sashank/springsecurityembarkx/SpringsecurityembarkxApplication.java
@@ -0,0 +1,13 @@
+package com.sashank.springsecurityembarkx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityembarkxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityembarkxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sashank/springsecurityembarkx/SpringsecurityembarkxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sashank.springsecurityembarkx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityembarkxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
